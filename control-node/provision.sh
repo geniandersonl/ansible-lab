9466432d245e882657af5bf223948f343e987b3f
Genianderson de Lima <geniandersonl@gmail.com>
2022-08-28 15:56:06 -0300

    fix some files

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -3,7 +3,8 @@
 sudo yum -y install epel-release
 echo "Inicio da instalacao do Ansible"
 sudo yum -y install ansible
-cat <<EOT >> /etc/hosts
+#ansible-galaxy install geerlingguy.mysql
+cat <<EOT >>/etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
 192.168.1.4 db1
