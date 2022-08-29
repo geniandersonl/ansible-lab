d05d707d57bbc877c2419b150bdfdd740cb0873c
Genianderson de Lima <geniandersonl@gmail.com>
2022-08-29 02:04:30 -0300

    fixe files

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -7,7 +7,7 @@ sudo yum -y install ansible
 cat <<EOT >>/etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
-192.168.1.4 db1
+192.168.1.4 db01
 EOT
 
 cat <<EOF >>/home/vagrant/.ssh/authorized_keys
