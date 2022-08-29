ab0856517bb404268c5a7008d6c1c81c31dada4d
Genianderson de Lima <geniandersonl@gmail.com>
2022-08-29 10:41:59 -0300

    fixes files

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+cp /vagrant/certs/id_rsa* /home/vagrant/.ssh/
+chown vagrant: /home/vagrant/.ssh/id_rsa*
+
 sudo yum -y install epel-release
 echo "Inicio da instalacao do Ansible"
 sudo yum -y install ansible
 #ansible-galaxy install geerlingguy.mysql
-
-ssh-keygen -t rsa -P '' -b 4096 -f /home/vagrant/.ssh/id_rsa -C vagrant-control-node
-chown vagrant: /home/vagrant/.ssh/id_rsa*
+cat <<EOT >>/etc/hosts
+192.168.1.2 control-node
+192.168.1.3 app01
+192.168.1.4 db01
+EOT
